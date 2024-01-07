1aefd321e657c2ce2ef4e4373e26410d199da0c5
milhet <benjamin.milhet@outlook.fr>
2024-01-07 22:12:18 +0100

    ajout message de prevention

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -11,5 +11,27 @@ export class AppComponent implements  OnInit{
 
   ngOnInit() {
     this.dateMort = localStorage.getItem('date_mort');
+    this.showModal();
+  }
+
+  showModal(): void {
+    const modal = document.getElementById('warningModal');
+    const span = document.getElementById('closeModal');
+
+    if (modal && span) {
+      modal.style.display = 'block';
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = 'none';
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target === modal) {
+          modal.style.display = 'none';
+        }
+      }
+    }
   }
 }
